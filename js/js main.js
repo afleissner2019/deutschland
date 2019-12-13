c4c531f009d1ca1415e742e7226b49d20aa07d31
afleissner2019 <58853796+afleissner2019@users.noreply.github.com>
2019-12-13 15:28:31 +0100

    create js

---
+++ js/js main.js
@@ -0,0 +1,20 @@
+function aboutClick(){
+    // hiding home section
+    document.querySelector("#homeSection").classList.add("hidden");
+    document.querySelector("#aboutSection").classList.remove("hidden");
+    document.querySelector("#contactSection").classList.add("hidden");
+}
+
+function contactClick(){
+    // hiding home section
+    document.querySelector("#homeSection").classList.add("hidden");
+    document.querySelector("#aboutSection").classList.add("hidden");
+    document.querySelector("#contactSection").classList.remove("hidden");
+}
+
+function homeClick(){
+    // hiding home section
+    document.querySelector("#homeSection").classList.remove("hidden");
+    document.querySelector("#contactSection").classList.add("hidden");
+    document.querySelector("#aboutSection").classList.add("hidden");
+}
